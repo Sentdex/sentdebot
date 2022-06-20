0e00f0e9e461e850af87d26a7906af401259eb1c
Alyce Osbourne <39281408+AlyceOsbourne@users.noreply.github.com>
2022-06-20 13:10:33 +0000

    more replit tweaks

--- sentdebot.py
+++ sentdebot.py
@@ -5,8 +5,13 @@ from discord.ext import commands
 
 from bot_config import BotConfig
 
+
 bot_config: BotConfig = BotConfig.get_config('sentdebot')
 client = commands.Bot(command_prefix=bot_config.prefix, intents=discord.Intents.all())
+
+# if .replit.txt exists token = token = os.environ['token'] else bot_config.token
+
+
 
 # drop the help command
 client.remove_command('help')
@@ -42,6 +47,7 @@ if not os.path.exists('cogs'):
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
+
 
 
 client.run(bot_config.token)
