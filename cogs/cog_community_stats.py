d229bf87e2884d972f34694ceedd7ede788b0dba
Alyce <Alyceosbourne@outlook.com>
2022-06-20 23:22:48 +0100

    can now search a defined channel

--- cogs/cog_community_stats.py
+++ cogs/cog_community_stats.py
@@ -67,21 +67,31 @@ class CommunityStats(commands.Cog):
         file = discord.File(os.path.join(bot_config.path, 'activity.png'), filename='activity.png')
         await ctx.send(file=file)
 
-    async def user_metrics_background_task(self):
-        await self.bot.wait_until_ready()
-        guild = self.bot.get_guild(bot_config.guild_id)
 
-        while not self.bot.is_closed():
+    async def user_metrics_background_task(self):
+        client = self.bot
+
+        await client.wait_until_ready()
+        sentdex_guild = client.get_guild(bot_config.guild_id)
+        path = bot_config.path
+        DAYS_BACK = bot_config.days_to_keep
+        RESAMPLE = bot_config.resample_interval
+        MOST_COMMON_INT = bot_config.top_user_count
+        COMMUNITY_BASED_CHANNELS = [channel.id for channel in get_all_channels_by_tag('community')]
+        HELP_CHANNELS = [channel.id for channel in get_all_channels_by_tag('help')]
+        DISCORD_BG_COLOR = '#36393E'
+
+        while not client.is_closed():
             try:
-                online, idle, offline = self.__community_report(guild)
-                with open(f"{bot_config.path}/usermetrics.csv", "a") as f:
+                online, idle, offline = self.__community_report(sentdex_guild)
+                with open(f"{path}/usermetrics.csv", "a") as f:
                     f.write(f"{int(time.time())},{online},{idle},{offline}\n")
 
-                df_msgs = pd.read_csv(f'{bot_config.path}/msgs.csv', names=['time', 'uid', 'channel'])
-                df_msgs = df_msgs[(df_msgs['time'] > time.time() - (86400 * bot_config.days_to_keep))]
+                df_msgs = pd.read_csv(f'{path}/msgs.csv', names=['time', 'uid', 'channel'])
+                df_msgs = df_msgs[(df_msgs['time'] > time.time() - (86400 * DAYS_BACK))]
                 df_msgs['count'] = 1
                 df_msgs['date'] = pd.to_datetime(df_msgs['time'], unit='s')
-                df_msgs.drop(labels="time", axis=1, inplace=True)
+                df_msgs.drop("time", 1, inplace=True)
                 df_msgs.set_index("date", inplace=True)
 
                 df_no_dup = df_msgs.copy()
@@ -90,34 +100,29 @@ class CommunityStats(commands.Cog):
 
                 df_no_dup.dropna(inplace=True)
 
-                message_volume = df_msgs["count"].resample(bot_config.resample_interval).sum()
+                message_volume = df_msgs["count"].resample(RESAMPLE).sum()
 
                 user_id_counts_overall = Counter(
-                    df_no_dup[
-                        df_no_dup['channel'].isin([channel.id for channel in get_all_channels_by_tag('community')])][
-                        'uid'].values).most_common(
-                    bot_config.top_user_count)
+                    df_no_dup[df_no_dup['channel'].isin(COMMUNITY_BASED_CHANNELS)]['uid'].values).most_common(
+                    MOST_COMMON_INT)
                 # print(user_id_counts_overall)
 
-                uids_in_help = Counter(
-                    df_no_dup[df_no_dup['channel'].isin([channel.id for channel in get_all_channels_by_tag('help')])][
-                        'uid'].values).most_common(
-                    bot_config.top_user_count)
+                uids_in_help = Counter(df_no_dup[df_no_dup['channel'].isin(HELP_CHANNELS)]['uid'].values).most_common(
+                    MOST_COMMON_INT)
                 # print(uids_in_help)
 
-                df = pd.read_csv(f"{bot_config.path}/usermetrics.csv", names=['time', 'online', 'idle', 'offline'])
-                df = df[(df['time'] > time.time() - (86400 * bot_config.days_to_keep))]
+                df = pd.read_csv(f"{path}/usermetrics.csv", names=['time', 'online', 'idle', 'offline'])
+                df = df[(df['time'] > time.time() - (86400 * DAYS_BACK))]
                 df['date'] = pd.to_datetime(df['time'], unit='s')
                 df['total'] = df['online'] + df['offline'] + df['idle']
-                df.drop(labels="time", axis=1, inplace=True)
+                df.drop("time", 1, inplace=True)
                 df.set_index("date", inplace=True)
 
-                df = df.resample(bot_config.resample_interval).mean()
+                df = df.resample(RESAMPLE).mean()
                 df = df.join(message_volume)
 
                 df.dropna(inplace=True)
                 # print(df.head())
-                DISCORD_BG_COLOR = '#36393E'
 
                 fig = plt.figure(facecolor=DISCORD_BG_COLOR)
                 ax1 = plt.subplot2grid((2, 1), (0, 0))
@@ -148,25 +153,26 @@ class CommunityStats(commands.Cog):
 
                 plt.subplots_adjust(left=0.11, bottom=0.10, right=0.89, top=0.95, wspace=0.2, hspace=0)
                 ax1.get_xaxis().set_visible(False)
+
                 ax1v.set_ylim(0, 3 * df["count"].values.max())
 
                 # plt.show()
-                plt.savefig(f"{bot_config.path}/online.png", facecolor=fig.get_facecolor())
+                plt.savefig(f"{path}/online.png", facecolor=fig.get_facecolor())
                 plt.clf()
 
                 fig = plt.figure(facecolor=DISCORD_BG_COLOR)
                 ax1 = plt.subplot2grid((2, 1), (0, 0))
 
                 plt.xlabel("Message Volume")
-                plt.title(f"General User Activity (past {bot_config.days_to_keep} days)")
+                plt.title(f"General User Activity (past {DAYS_BACK} days)")
                 ax1.set_facecolor(DISCORD_BG_COLOR)
 
                 users = []
                 msgs = []
                 for pair in user_id_counts_overall[::-1]:
                     try:
-                        users.append(guild.get_member(pair[0]).name)  # get member name from here
-                        if "Dhanos" in guild.get_member(pair[0]).name:
+                        users.append(sentdex_guild.get_member(pair[0]).name)  # get member name from here
+                        if "Dhanos" in sentdex_guild.get_member(pair[0]).name:
                             msgs.append(pair[1] / 1.0)
                         else:
                             msgs.append(pair[1])
@@ -177,7 +183,7 @@ class CommunityStats(commands.Cog):
                 plt.yticks(y_pos, users)
 
                 ax2 = plt.subplot2grid((2, 1), (1, 0))
-                plt.title(f"Help Channel Activity (past {bot_config.days_to_keep} days)")
+                plt.title(f"Help Channel Activity (past {DAYS_BACK} days)")
                 plt.xlabel("Help Channel\nMsg Volume")
                 ax2.set_facecolor(DISCORD_BG_COLOR)
 
@@ -185,9 +191,9 @@ class CommunityStats(commands.Cog):
                 msgs = []
                 for pair in uids_in_help[::-1]:
                     try:
-                        users.append(guild.get_member(pair[0]).name)  # get member name from here
+                        users.append(sentdex_guild.get_member(pair[0]).name)  # get member name from here
 
-                        if "Dhanos" in guild.get_member(pair[0]).name:
+                        if "Dhanos" in sentdex_guild.get_member(pair[0]).name:
                             msgs.append(pair[1] / 1.0)
                         else:
                             msgs.append(pair[1])
@@ -200,25 +206,14 @@ class CommunityStats(commands.Cog):
                 plt.yticks(y_pos, users)
 
                 plt.subplots_adjust(left=0.30, bottom=0.15, right=0.99, top=0.95, wspace=0.2, hspace=0.55)
-                plt.savefig(f"{bot_config.path}/activity.png", facecolor=fig.get_facecolor())
+                plt.savefig(f"{path}/activity.png", facecolor=fig.get_facecolor())
                 plt.clf()
+
                 await asyncio.sleep(300)
 
-            except FileNotFoundError as e:
-                # create file if it doesn't exist
-                print(str(e))
-                if not os.path.exists(f"{bot_config.path}/usermetrics.csv"):
-                    with open(f"{bot_config.path}/usermetrics.csv", "w") as f:
-                        f.write("")
-                # if msgs don't exist, create them
-                if not os.path.exists(f"{bot_config.path}/msgs.csv"):
-                    with open(f"{bot_config.path}/msgs.csv", "w") as f:
-                        f.write("")
-                if not os.path.exists(f"{bot_config.path}/logs.csv"):
-                    with open(f"{bot_config.path}/logs.csv", "w") as f:
-                        f.write("")
             except Exception as e:
-                raise e
+                print(str(e))
+                await asyncio.sleep(300)
 
 def setup(bot):
     bot.add_cog(CommunityStats(bot))
