e3a4a8e79f9cbd4126fecd96c66e113902b56a8a
Alyce <Alyceosbourne@outlook.com>
2022-06-21 13:50:00 +0100

    OK, tweaked some things, adding some test features

--- cogs/cog_daily_challenge.py
+++ cogs/cog_daily_challenge.py
@@ -9,27 +9,19 @@ class DailyChallenge(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-    def get_daily_challenge(self):
-        soup = BeautifulSoup(requests.get('https://projecteuler.net/recent').text, 'html.parser')
-        href = soup.find('table', {'id': 'problems_table'}).find_all('tr')[1].find('a')['href']
-        return f'https://projecteuler.net/{href}'
-
-    # daily loop
     @tasks.loop(seconds=86400)
     async def daily_challenge(self):
         await self.bot.wait_until_ready()
         channels = discord.utils.get(self.bot.get_all_channels(), name='daily-challenge')
-        if channels is None:
-            return
-        challenge = self.get_daily_challenge()
-        for channel in channels:
-            messages = await channel.history(limit=100).flatten()
-            for message in messages:
+        if channels is not None:
+            soup = BeautifulSoup(requests.get('https://projecteuler.net/recent').text, 'html.parser')
+            href = soup.find('table', {'id': 'problems_table'}).find_all('tr')[1].find('a')['href']
+            challenge = f'https://projecteuler.net/{href}'
+            async for message in channels.history(limit=100):
                 if message.author == self.bot.user:
                     if message.content.startswith('Here is your daily challenge!'):
                         return
-            await channel.send(f'Here is your daily challenge! {challenge}')
+            await channels.send("Here is your daily challenge!", embed=discord.Embed(description=challenge))
 
 
 def setup(bot):
