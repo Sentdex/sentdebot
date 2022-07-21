056255059f3e94a1d3ec34d747f6a36424bdad12
Martin Dousa <martindousa186@gmail.com>
2022-07-21 23:01:39 +0200

    removed dummy function

--- cogs/common.py
+++ cogs/common.py
@@ -128,9 +128,5 @@ class Common(Base_Cog):
   async def pet(self, inter: disnake.ApplicationCommandInteraction):
     await inter.response.send_modal(VoteSetupModal())
 
-  @commands.command()
-  async def send_message(self, ctx: commands.Context, message: str, channel: disnake.TextChannel):
-    await channel.send(message)
-
 def setup(bot):
   bot.add_cog(Common(bot))
